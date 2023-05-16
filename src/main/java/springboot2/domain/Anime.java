8a1e8d3e5bdc932a57bfea4223288d55ba270e74
Paulo Henrique dos Santos Eiterer <pheiterer@hotmail.com>
2023-05-16 18:56:08 -0300

    JPA:
    Including JPA and msql connector 🍃

--- src/main/java/springboot2/domain/Anime.java
+++ src/main/java/springboot2/domain/Anime.java
@@ -1,11 +1,18 @@
 package springboot2.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
 @AllArgsConstructor
+@Entity
 public class Anime {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
 
