0b0daf4d8a9f15e2967cead49cd7f19b339e3759
Paulo Henrique dos Santos Eiterer <pheiterer@hotmail.com>
2023-05-15 22:11:20 -0300

    POST method:
    Add PostMapping and using Postman to add another anime in list (only while app is running)🍃

--- src/main/java/springboot2/controller/AnimeController.java
+++ src/main/java/springboot2/controller/AnimeController.java
@@ -2,11 +2,9 @@ package springboot2.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import springboot2.domain.Anime;
 import springboot2.service.AnimeService;
 import springboot2.util.DateUtil;
@@ -31,6 +29,11 @@ public class AnimeController {
     @GetMapping(path = "/{id}")
     public ResponseEntity<Anime> findById(@PathVariable long id) {
         return ResponseEntity.ok(animeService.findById(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<Anime> save(@RequestBody Anime anime){
+        return new ResponseEntity<>(animeService.save(anime), HttpStatus.CREATED);
     }
 
 }
