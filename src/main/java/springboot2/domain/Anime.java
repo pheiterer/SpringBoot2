4169ca062ae9f9c9f88e538049f6312c3fc69974
Paulo Henrique dos Santos Eiterer <pheiterer@hotmail.com>
2023-05-16 21:03:13 -0300

    JPA:
    Change the methods for JPA 🍃

--- src/main/java/springboot2/domain/Anime.java
+++ src/main/java/springboot2/domain/Anime.java
@@ -5,15 +5,18 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 @Entity
+@Builder
 public class Anime {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
-
 }
