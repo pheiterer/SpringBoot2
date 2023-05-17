75ce8821690a7885888e24803f82298242bf6da8
Paulo Henrique dos Santos Eiterer <pheiterer@hotmail.com>
2023-05-16 23:32:15 -0300

    MapStruct Framework:
    Make the AnimeController more clean, and help in request control (but was really hard to config) 🍃

---
+++ src/main/java/springboot2/mapper/AnimeMapper.java
@@ -0,0 +1,16 @@
+package springboot2.mapper;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+import springboot2.domain.Anime;
+import springboot2.requests.AnimePostRequestBody;
+import springboot2.requests.AnimePutRequestBody;
+
+@Mapper(componentModel = "spring")
+public abstract class AnimeMapper {
+    public static final AnimeMapper INSTANCE = Mappers.getMapper(AnimeMapper.class);
+
+    public abstract Anime toAnime(AnimePostRequestBody animePostRequestBody);
+
+    public abstract Anime toAnime(AnimePutRequestBody animePostRequestBody);
+}
