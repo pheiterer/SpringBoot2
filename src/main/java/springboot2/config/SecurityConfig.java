70d998226bcfffa1896118465d1449ecd48f0bd9
Paulo Henrique dos Santos Eiterer <pheiterer@hotmail.com>
2023-05-23 16:48:19 -0300

    Spring Security (PreAuthori):
    Restricting access for method save to only ADMIN users🍃

--- src/main/java/springboot2/config/SecurityConfig.java
+++ src/main/java/springboot2/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package springboot2.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -14,6 +15,7 @@ import static org.springframework.security.config.Customizer.withDefaults;
 import static org.springframework.security.crypto.factory.PasswordEncoderFactories.createDelegatingPasswordEncoder;
 
 @Configuration
+@EnableMethodSecurity
 public class SecurityConfig {
 
     @Bean
--- src/main/java/springboot2/controller/AnimeController.java
+++ src/main/java/springboot2/controller/AnimeController.java
@@ -7,6 +7,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import springboot2.domain.Anime;
 import springboot2.requests.AnimePostRequestBody;
@@ -43,6 +44,7 @@ public class AnimeController {
     }
 
     @PostMapping
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<Anime> save(@RequestBody @Valid AnimePostRequestBody animePostRequestBody){
         return new ResponseEntity<>(animeService.save(animePostRequestBody), HttpStatus.CREATED);
     }
