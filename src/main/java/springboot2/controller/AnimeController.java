65f28d92b28b02968c49ce8a9ef4cb2391ea147c
Paulo Henrique dos Santos Eiterer <pheiterer@hotmail.com>
2023-05-23 20:47:57 -0300

    Spring Doc:
    Including SpringDoc OpenAPI 🍃

--- src/main/java/springboot2/controller/AnimeController.java
+++ src/main/java/springboot2/controller/AnimeController.java
@@ -3,6 +3,7 @@ package springboot2.controller;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springdoc.core.annotations.ParameterObject;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
@@ -25,12 +26,12 @@ public class AnimeController {
     private final AnimeService animeService;
 
     @GetMapping
-    public ResponseEntity<Page<Anime>> list(Pageable pageable) {
+    public ResponseEntity<Page<Anime>> list(@ParameterObject Pageable pageable) {
         return ResponseEntity.ok(animeService.listAll(pageable));
     }
 
     @GetMapping(path = "/all")
-    public ResponseEntity<List<Anime>> listAll() {
+    public ResponseEntity<List<Anime>> listAll( ) {
         return ResponseEntity.ok(animeService.listAllNoPageable());
     }
 
