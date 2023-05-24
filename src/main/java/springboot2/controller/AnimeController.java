409b7b7b9a3d0132021d7b16e29a58bb6d30cfbc
Paulo Henrique dos Santos Eiterer <pheiterer@hotmail.com>
2023-05-24 15:56:11 -0300

    Spring Doc:
    Put some details in SpringDoc OpenAPI 🍃

--- src/main/java/springboot2/controller/AnimeController.java
+++ src/main/java/springboot2/controller/AnimeController.java
@@ -1,5 +1,8 @@
 package springboot2.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
@@ -26,6 +29,8 @@ public class AnimeController {
     private final AnimeService animeService;
 
     @GetMapping
+    @Operation(summary = "List all animes paginated", description = "The dedault size is 20, use the parameter size to change the default value",
+    tags = {"animes"})
     public ResponseEntity<Page<Anime>> list(@ParameterObject Pageable pageable) {
         return ResponseEntity.ok(animeService.listAll(pageable));
     }
@@ -58,6 +63,10 @@ public class AnimeController {
     }
 
     @DeleteMapping(path = "/admin/{id}")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "Successful Operation"),
+            @ApiResponse(responseCode = "400", description = "When Anime Does Not Exist In The Database")
+    })
     public ResponseEntity<Void> delete(@PathVariable long id) {
         animeService.delete(id);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
--- src/main/java/springboot2/requests/AnimePostRequestBody.java
+++ src/main/java/springboot2/requests/AnimePostRequestBody.java
@@ -1,5 +1,6 @@
 package springboot2.requests;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -12,5 +13,6 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class AnimePostRequestBody {
     @NotEmpty(message = "The anime name cannot be empty")
+    @Schema(description = "This is the Anime's name", example = "Tensei Shittara Slime Datta Ken")
     private String name;
 }
